ae5c0e29ae99e8ee5a30b180044297081db2a8a3
Zhenlong Zhao <zhanzhenlong.dev@gmail.com>
2017-10-29 21:35:29 +0700

    yield when set a lower priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -338,7 +338,10 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
+  int prev_priority = thread_current ()->priority;
   thread_current ()->priority = new_priority;
+  if (new_priority < prev_priority)
+    thread_yield();
 }
 
 /* Returns the current thread's priority. */
